f6dc5cdbc8953ea3420791c2e057172ca2ad24b0
SonicRaptor <sonicraptor2105@gmail.com>
2026-02-16 21:29:03 +1100

    continued working on checkLatestCommit()

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def createGit():
 
 def blobFile(f):
     data = f.read()
-    header = f"BLOB {len(data)}\0".encode('utf-8')
+    header = f"1 {len(data)}\0".encode('utf-8')
     dataType = 0 #Initialise one Byte for file type
     dataType = 2 if os.access(f.name, os.X_OK) else 1 #Set to 2 if its executable else set to 1
     print(dataType)
@@ -42,7 +42,7 @@ def makeTrees(directory):
     for x in blobs:
         finalTree.append(" ".join(map(str, x)))
     finalTreeConcat = ("\x00".join((finalTree))).encode("utf-8")
-    header = f"TREE {len(finalTreeConcat)}\0".encode("utf-8")
+    header = f"2 {len(finalTreeConcat)}\0".encode("utf-8")
     finalHash = compressSave(header, finalTreeConcat)
     print(finalTreeConcat.decode("utf-8"))
     try:
@@ -62,19 +62,27 @@ def compressSave(header, finalBlob):
 
 #WIP
 def makeCommit(treeHash, author, comment):
-    previousCommit = str()
-    data = "\n".join([previousCommit, treeHash, author, datetime.now(timezone.utc).strftime("%d/%m/%y %H:%M+00:00"), comment]).encode("utf-8")
-    header = f"COMM {len(data)}\0".encode("utf-8")
+    previousCommit = "str()"
+    data = " ".join([previousCommit, treeHash, author, datetime.now(timezone.utc).strftime("%d/%m/%y %H:%M+00:00"), comment]).encode("utf-8")
+    header = f"3 {len(data)}\0".encode("utf-8")
     compressSave(header, data)
 
 def checkLatestCommit():
-    if not os.path.exists(f"{os.curdir}/.xgit"):
-        return #First commit
-    with os.scandir(f"{os.curdir}/.xgit/") as folders:
-        if folders.is_dir():
-            for f in folders: #Checking the contents of each hash folder
-                with open(f) as d:
-                    if d.readlines.decode("utf-8"):
+    if not os.path.exists(f"{os.curdir}/.xgit/cache"):
+        commits = list()
+        if not os.path.exists(f"{os.curdir}/.xgit"):
+            return #First commit
+        with os.scandir(f"{os.curdir}/.xgit/") as folders:
+            for files in folders:
+                if files.is_dir():
+                    for f in os.scandir(files): #Checking the contents of each hash folder
+                        with open(f, "rb") as d:
+                            data = zlib.decompress(d.read()).split(b"\0") #Decompress and split into header [0] and data [1]
+                            print(data)
+                            if data[0][0] == ord("3"):
+                                commits.append(data[1].decode("utf-8").partition(" ")[0])
+                                print(commits)
+
 
 
 
@@ -88,4 +96,18 @@ except OSError:
     ignoreList = createIgnoreFile()
 
 makeCommit('abc', 'x', 'Test Commit')
+checkLatestCommit()
 #makeTrees(os.curdir)
+
+
+
+#DATA LIST:
+#FILE TYPES:
+#1 = File
+#2 = EXE
+#10 = Folder
+
+#COMPRESSED FILE TYPES:
+#1 = BLOB
+#2 = TREE
+#3 = COMMIT
