0024eba27848f51a16a8043bb5acf2f09556dd13
Matthew <matthew.hamilton@2026.arden.nsw.edu.au>
2026-02-17 06:15:34 +0000

    finished orderCommitList and makeCommit functions

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def blobFile(f):
     header = f"1 {len(data)}\0".encode('utf-8')
     dataType = 0 #Initialise one Byte for file type
     dataType = 2 if os.access(f.name, os.X_OK) else 1 #Set to 2 if its executable else set to 1
-    print(dataType)
     finalHash = compressSave(header, data)
     return (dataType, finalHash, f.name) #Returns data needed for a tree
 
@@ -32,11 +31,9 @@ def makeTrees(directory):
     with os.scandir(directory) as files:
         for file in files:
             if file.is_file() and not file.name in ignoreList:
-                print(f"file {file.name}")
                 with open(file, 'rb') as fileObj:
                     blobs.append(blobFile(fileObj))
             elif file.is_dir() and not file.name in ignoreList:
-                print(f"folder {file.name}")
                 blobs.append(makeTrees(file.path))
     finalTree = list()
     for x in blobs:
@@ -44,10 +41,8 @@ def makeTrees(directory):
     finalTreeConcat = ("\x00".join((finalTree))).encode("utf-8")
     header = f"2 {len(finalTreeConcat)}\0".encode("utf-8")
     finalHash = compressSave(header, finalTreeConcat)
-    print(finalTreeConcat.decode("utf-8"))
     try:
         fileName = directory.rsplit("\\", 1)[1]
-        print(fileName)
     except: #Parent directory
         fileName = "."
     return (10, finalHash, fileName) #Returns data needed for a tree to link to a new tree
@@ -62,10 +57,11 @@ def compressSave(header, finalBlob):
 
 #WIP
 def makeCommit(treeHash, author, comment):
-    previousCommit = "str()"
+    previousCommit = "" if checkLatestCommit() == None else checkLatestCommit()
     data = " ".join([previousCommit, treeHash, author, datetime.now(timezone.utc).strftime("%d/%m/%y %H:%M+00:00"), comment]).encode("utf-8")
     header = f"3 {len(data)}\0".encode("utf-8")
-    compressSave(header, data)
+    commitHash = compressSave(header, data)
+    with open(f"{os.curdir}/.xgit/cache", "wb") as f: f.write(commitHash.encode("utf-8"))
 
 def checkLatestCommit():
     if not os.path.exists(f"{os.curdir}/.xgit/cache"):
@@ -78,10 +74,39 @@ def checkLatestCommit():
                     for f in os.scandir(files): #Checking the contents of each hash folder
                         with open(f, "rb") as d:
                             data = zlib.decompress(d.read()).split(b"\0") #Decompress and split into header [0] and data [1]
-                            print(data)
-                            if data[0][0] == ord("3"):
-                                commits.append(data[1].decode("utf-8").partition(" ")[0])
-                                print(commits)
+                            if data[0][0] == ord("3"): #Checking if the type is commit by checking the first byte
+                                commits.append({f.name : data[1].decode("utf-8").partition(" ")[0]}) #Decode the data and seperate the previous hash from the data then return the Commits Hash : Previous Commits Hash
+        if len(commits) == 0:
+            return #Initial commit
+        return orderCommitList(commits)[0] #Return most recent commit in the list
+    with open(f"{os.curdir}/.xgit/cache", "rb") as f:
+        data = f.read().decode("utf-8")
+    if len(data) == 40:
+        print("true")
+        return data
+    else:
+        print(" - Cache has been modified or corrupted. Regenerating")
+        os.remove(f"{os.curdir}/.xgit/cache")
+        return checkLatestCommit()
+
+def orderCommitList(commits): #WIP Needs error checking still
+    finalOrder = list()
+    currentHash = str()
+    for d in commits[:]:
+        if "" == list(d.values())[0]:
+            currentHash = list(d.keys())[0]
+            finalOrder.append(currentHash)
+            commits.remove(d)
+            break
+
+    while len(commits) > 0:
+        for d in commits[:]:
+            if currentHash == list(d.values())[0]:
+                currentHash = list(d.keys())[0]
+                finalOrder.insert(0, currentHash)
+                commits.remove(d)
+                break
+    return(finalOrder)
 
 
 
@@ -90,14 +115,11 @@ createGit()
 ignoreList = list()
 try:
     with open(f"{os.curdir}/.xgit/ignore.dat", 'r') as f: ignoreList = f.read().split("\n")
-    print(ignoreList)
 except OSError:
     print(" - Regenerating ignore.dat")
     ignoreList = createIgnoreFile()
 
-makeCommit('abc', 'x', 'Test Commit')
-checkLatestCommit()
-#makeTrees(os.curdir)
+makeCommit(makeTrees(os.curdir)[1], 'x', 'Test Commit')
 
 
 
